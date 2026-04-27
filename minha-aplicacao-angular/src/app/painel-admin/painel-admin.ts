c684f5ff631a65811a44d0a119e47b4cab4722ee
Gustavo Faroni <116437253+GustavoFaroni@users.noreply.github.com>
2026-04-27 19:42:41 -0300

    dever de casa Aula1

---
+++ minha-aplicacao-angular/src/app/painel-admin/painel-admin.ts
@@ -0,0 +1,13 @@
+
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-painel-adm',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './painel-adm.html',
+  styleUrl: './painel-adm.css'
+})
+export class PainelAdm {}
+          
