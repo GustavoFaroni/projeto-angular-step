0b1bf10fd9c46c40b3a59a9600fe7c8185d149f5
Gustavo Faroni <116437253+GustavoFaroni@users.noreply.github.com>
2026-05-04 19:30:35 -0300

    aula 4 navegacao

--- minha-aplicacao-angular/src/app/app.routes.ts
+++ minha-aplicacao-angular/src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+// import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'produtos',
+    pathMatch: 'full',
+  },
+  {
+    path: 'produtos',
+    loadComponent: () =>
+      import('./pages/lista-produtos/lista-produtos').then(
+        (m) => m.ListaProdutos
+      ),
+  },
+  {
+    path: 'produto/:id',
+    loadComponent: () =>
+      import('./pages/detalhe-produto/detalhe-produto').then(
+        (m) => m.DetalheProduto
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'produtos', 
+  }
+];
+          
